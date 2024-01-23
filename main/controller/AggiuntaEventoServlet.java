05d8f081f806a4c81c9c501d442d0a1ce3a7f51c
Tre02 <tresy.sorrentino02@gmail.com>
2024-01-23 19:31:01 +0100

    Servlet AggiuntaEventoServlet.java

---
+++ main/controller/AggiuntaEventoServlet.java
@@ -0,0 +1,36 @@
+package controller;
+
+import model.Evento;
+import model.EventoDAO;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet({"/AggiuntaEventoServlet"})
+public class AggiuntaEventoServlet extends HttpServlet {
+    public AggiuntaEventoServlet() {
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Evento p = new Evento();
+        p.setIdEvento(request.getParameter("ID_evento"));
+        p.setNomeEvento(request.getParameter("Nome_evento"));
+        p.setPostidisponibili(request.getIntHeader("posti_disponibili"));
+        p.setData(request.getParameter("data"));
+        p.setLuogo(request.getParameter("luogo"));
+        p.setOra(request.getIntHeader("ora"));
+        p.setPrezzo(request.getIntHeader("prezzo"));
+        p.setTipo(request.getParameter("tipo"));
+        EventoDAO.aggiuntaEvento(p);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("HomeServletAmministratore");
+        dispatcher.forward(request, response);
+    }
+}
