bf8a28b503b83a43039f0995b42d74ff1f1c4948
frigo <soranicolo@duck.com>
2024-02-19 16:24:27 +0100

    Testing

---
+++ Ticket4U/src/test/LoginServletTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LoginServletTest {
+
+    private static LoginServletSupport login;
+
+    @BeforeAll
+    public static void init() {
+         login = Mockito.mock(LoginServletSupport.class);
+    }
+
+    @Test
+    public void LoginServletTest1(){
+        String email = "n.sorà@libero.it";
+        String password = "nsora1001";
+        Mockito.when(login.validateLogin(email, password)).thenReturn(true);
+        assertTrue(login.validateLogin(email, password));
+    }
+    @Test
+    public void LoginServletTest2(){
+        String email = "n.sorà@libero.it";
+        String password = "nsora1001";
+        Mockito.when(login.validateLogin(email, password)).thenReturn(false);
+        assertFalse(login.validateLogin(email, password));
+    }
+}
