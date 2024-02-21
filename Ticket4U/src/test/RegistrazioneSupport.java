10db3fc26ba86d8bd83a850f01107e7b5a9b4d9c
frigocaldo <91260529+frigocaldo@users.noreply.github.com>
2024-02-21 12:33:19 +0100

    bugfix: Testing

--- Ticket4U/src/test/RegistrazioneSupport.java
+++ Ticket4U/src/test/RegistrazioneSupport.java
@@ -1,8 +1,3 @@
-import java.time.LocalDate;
-import java.time.Period;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
 public class RegistrazioneSupport {
     public RegistrazioneSupport () {}
 
@@ -10,23 +5,6 @@ public class RegistrazioneSupport {
         return input.matches("^[a-zA-Z\\s]+$");
     }
 
-    public boolean validateDate (String date) {
-        String[] array = date.split("/");
-        int anno = Integer.parseInt(array[2]);
-        int mese = Integer.parseInt(array[1]);
-        int giorno = Integer.parseInt(array[0]);
-
-        LocalDate today = LocalDate.now();
-        LocalDate birthday = LocalDate.of(anno, mese, giorno);
-
-        Period p = Period.between(birthday, today);
-
-        if(p.getYears() < 18) {
-            return false;
-        } else {
-            return true;
-        }
-    }
 
     public boolean validateSyntaxEmail (String email) {
         return email.matches("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$");
@@ -44,19 +22,13 @@ public class RegistrazioneSupport {
     public boolean validateSignup(String name, String surname) {
         return validateSyntaxNameSurname(name) && validateSyntaxNameSurname(surname);
     }
-    public boolean validateSignup(String name, String surname,String ddn) {
+    public boolean validateSignup(String name, String surname,String email) {
         return validateSyntaxNameSurname(name) && validateSyntaxNameSurname(surname)
-                && validateDate(ddn);
+                && validateSyntaxEmail(email);
     }
 
     public boolean validateSignup(String name, String surname,String email, String password) {
         return validateSyntaxNameSurname(name) && validateSyntaxNameSurname(surname)
                 && validateSyntaxEmail(email) && validateLenghtPassword(password);
-    }
-
-    public boolean validateSignup(String name, String surname, String ddn, String email, String password) {
-        return validateSyntaxNameSurname(name) && validateSyntaxNameSurname(surname)
-                && validateDate(ddn) && validateSyntaxEmail(email)
-                && validateLenghtPassword(password);
     }
 }
